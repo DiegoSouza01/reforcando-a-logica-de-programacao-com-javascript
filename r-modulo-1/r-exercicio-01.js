7e35555773cf6a9d54d83ab49dbcc924b7bee1a8
Diego <diegodecstraid@gmail.com>
2025-05-30 23:55:56 -0300

    Resolução do exercicio 01

--- r-modulo-1/r-exercicio-01.js
+++ r-modulo-1/r-exercicio-01.js
@@ -3,3 +3,19 @@
 */
 
 const prompt = require('prompt-sync')();
+
+// Entrada de dados: quantidade de cigarros fumados por dia e anos fumando
+let cigarrosPorDia = parseInt(prompt('Quantos cigarros você fuma por dia? '));
+let anosFumando = parseInt(prompt('Há quantos anos você fuma? '));
+
+// Cálculo do total de cigarros fumados
+let totalCigarros = cigarrosPorDia * 365 * anosFumando;
+
+// Cálculo da perda de vida em minutos
+let perdaVidaMinutos = totalCigarros * 10;
+
+// Cálculo da perda de vida em dias
+let perdaVidaDias = perdaVidaMinutos / (60 * 24);
+
+// Exibição do resultado
+console.log(`Você perderá aproximadamente ${Math.round(perdaVidaDias)} dias de vida devido ao fumo. Fume com moderação!`);
