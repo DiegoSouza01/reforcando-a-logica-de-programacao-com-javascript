e189f81d3a81051eb6f6b298703bbac6854d322b
Diego <diegodecstraid@gmail.com>
2025-06-07 21:40:02 -0300

    Resolução do Exercicio 12

--- r-modulo-1/r-exercicio-12.js
+++ r-modulo-1/r-exercicio-12.js
@@ -2,3 +2,19 @@
     Faça um programa que mostre os 10 primeiros elementos da Sequência de Fibonacci.
     Ex.: 1, 1, 2, 3, 5, 8, 13, 21.
 */
+
+// A sequência de Fibonacci é uma série de números onde cada número é a soma dos dois anteriores.
+function fibonacci(n) {
+    let bona = [1, 1];
+    for (let i = 2; i < n; i++) {
+        bona[i] = bona[i - 1] + bona[i - 2];
+    }
+    return bona;
+}
+
+// Exibe os 10 primeiros elementos da sequência de Fibonacci
+let n = 10;
+let fibonacciSequence = fibonacci(n);
+
+// Exibe a sequência de Fibonacci
+console.log(`Os ${n} primeiros elementos da Sequência de Fibonacci são: ${fibonacciSequence.join(', ')}`);
