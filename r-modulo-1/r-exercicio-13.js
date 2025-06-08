307521ed1dd6bf3d45e1d030d4cff184429c4c6b
Diego <diegodecstraid@gmail.com>
2025-06-07 23:41:41 -0300

    Resolução do Exercicio 13

--- r-modulo-1/r-exercicio-13.js
+++ r-modulo-1/r-exercicio-13.js
@@ -3,3 +3,19 @@
     atribuindo diretamente) um vetor numérico com 15 posições com os primeiros elementos
     da sequência de Fibonacci.
 */
+
+//Vetor para armazenar os elementos da sequência de Fibonacci
+let fibonacci = [];
+
+// Preenchendo o vetor com os primeiros 15 elementos da sequência de Fibonacci
+for (let i = 0; i < 15; i++) {
+    if (i === 0 || i === 1) {
+        fibonacci[i] = 1; // Os dois primeiros elementos são 1
+    } else {
+        fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2]; // Cada elemento é a soma dos dois anteriores
+    }
+}
+
+// Exibindo o vetor preenchido
+console.log(fibonacci);
+
