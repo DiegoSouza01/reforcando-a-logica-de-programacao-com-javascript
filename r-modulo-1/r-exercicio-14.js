69c2311d09b349388855ad8f1d97359da470660a
Diego <diegodecstraid@gmail.com>
2025-06-08 23:39:44 -0300

    Resolução do Exercicio 14

--- r-modulo-1/r-exercicio-14.js
+++ r-modulo-1/r-exercicio-14.js
@@ -3,3 +3,17 @@
     mostre uma listagem com todos os nomes informados, na ordem inversa daquela em
     que eles foram informados.
 */
+
+const prompt = require('prompt-sync')();
+
+// Vetor para armazenar os nomes
+let nomes = [];
+
+// Lendo 7 nomes de pessoas
+for (let i = 0; i < 7; i++) {
+    let nome = prompt(`Digite o nome da pessoa ${i + 1}:`);
+    nomes.push(nome);
+}
+
+// Exibindo os nomes na ordem inversa
+console.log(nomes.reverse());
